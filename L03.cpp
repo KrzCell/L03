d15d1cf4e9aad852e6c94cd40195f8b66aa2ddaf
Krzysztof Cel <krzycel789@student.polsl.pl>
2020-11-03 09:34:37 +0100

    parzysta

--- L03.cpp
+++ L03.cpp
@@ -1,9 +1,27 @@
 ﻿
 #include <iostream>
+using namespace std;
+
+#define Zadanie3A
+
+#ifdef Zadanie3A
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int n = 0, l = 0, liczba = 0;
+	do
+    {
+		cout << "Wprowadz liczbe ";
+		cin >> liczba;
+		if ((liczba & 1) == 0)
+			l++;
+		n++;
+	} while (l!=3);
+
+	
 }
+
+#endif // Zadanie3A
+
 
 
