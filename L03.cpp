f90b73c68cd507ac39049a47b85aba995e9a3b4d
Krzysztof Cel <krzycel789@student.polsl.pl>
2020-11-03 09:41:10 +0100

    silnia zad3A

--- L03.cpp
+++ L03.cpp
@@ -9,6 +9,7 @@ using namespace std;
 int main()
 {
 	int n = 0, l = 0, liczba = 0;
+	double silnia = 1;
 	do
     {
 		cout << "Wprowadz liczbe ";
@@ -17,7 +18,10 @@ int main()
 			l++;
 		n++;
 	} while (l!=3);
-
+	
+	for (int i = n; i > 1; i--)
+		silnia *= i;
+	cout << n << "! = " << silnia << endl;
 	
 }
 
